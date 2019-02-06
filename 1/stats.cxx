4616415e4789ecf4fe6ecae54ea07dc33090a115
sajad1993gh <sajad1993gh@gmail.com>
2019-02-05 22:51:04 -0500

    more

---
+++ 1/stats.cxx
@@ -0,0 +1,35 @@
+
+#include <iostream>
+#include <math.h>
+#include "newpoint.h"
+using namespace std;
+
+namespace main_savitch_2C
+{
+  statistician::statistician()
+  {
+    //count;
+    //total;
+    //tinyest;
+    //largest;
+  }
+
+  void statistician::next(double r)
+  {
+
+  }
+
+  void statistician::reset( )
+  {
+
+  }
+
+  
+
+
+
+
+
+
+
+}
