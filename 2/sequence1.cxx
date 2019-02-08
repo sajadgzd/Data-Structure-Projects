eb0b6ab52df8291a3c42ce8f60e01a782958cfd3
sajad1993gh <sajad1993gh@gmail.com>
2019-02-08 18:41:07 -0500

    more

---
+++ 2/sequence1.cxx
@@ -0,0 +1,20 @@
+// FILE: sequence1.cxx
+// CLASS IMPLEMENTED: sequence (see sequence1.h for documentation)
+// INVARIANT for the bag class:
+//   1. The number of items in the bag is in the member variable used;
+//   2. For an empty bag, we do not care what is stored in any of data; for a
+//      non-empty bag the items in the bag are stored in data[0] through
+//      data[used-1], and we don't care what's in the rest of data.
+
+#include <algorithm> // Provides copy function
+#include <cassert>   // Provides assert function
+#include "sequence1.h"
+using namespace std;
+
+namespace main_savitch_3
+{
+
+
+
+
+}
