85e2a9bd11f26e3aa97703029fb6cb95f11cfcbd
sajad1993gh <sajad1993gh@gmail.com>
2019-02-09 00:35:45 -0500

    more

--- 2/sequence1.cxx
+++ 2/sequence1.cxx
@@ -8,7 +8,6 @@
 //   3.If there is a current item, then it lies in data[current_index]; if there is
 //   no current item, then current_index equals used.
 
-#include <algorithm> // Provides copy function
 #include <cassert>   // Provides assert function
 #include "sequence1.h"
 using namespace std;
@@ -22,16 +21,10 @@ namespace main_savitch_3
     current_index = 0;
   }
   void sequence::start()
-  //     Postcondition: The first item on the sequence becomes the current item
-  //     (but if the sequence is empty, then there is no current item).
   {
     current_index = 0;
   }
   void sequence::advance()
-  //     Precondition: is_item returns true.
-  //     Postcondition: If the current item was already the last item in the
-  //     sequence, then there is no longer any current item. Otherwise, the new
-  //     current item is the item immediately after the original current item.
   {
     assert(is_item());
     current_index++;
@@ -39,11 +32,6 @@ namespace main_savitch_3
 
   }
   void sequence::insert(const value_type& entry)
-  //     Precondition: size( ) < CAPACITY.
-  //     Postcondition: A new copy of entry has been inserted in the sequence
-  //     before the current item. If there was no current item, then the new entry
-  //     has been inserted at the front of the sequence. In either case, the newly
-  //     inserted item is now the current item of the sequence.
   {
     assert(size() < CAPACITY);
     size_type i;
@@ -55,11 +43,6 @@ namespace main_savitch_3
     used++;
   }
   void sequence::attach(const value_type& entry)
-  //     Precondition: size( ) < CAPACITY.
-  //     Postcondition: A new copy of entry has been inserted in the sequence after
-  //     the current item. If there was no current item, then the new entry has
-  //     been attached to the end of the sequence. In either case, the newly
-  //     inserted item is now the current item of the sequence.
   {
     assert(size() < CAPACITY);
     size_type i;
@@ -72,9 +55,6 @@ namespace main_savitch_3
     used++;
   }
   void sequence::remove_current()
-  //     Precondition: is_item returns true.
-  //     Postcondition: The current item has been removed from the sequence, and the
-  //     item after this (if there is one) is now the new current item.
   {
     assert(is_item());
     size_type i;
@@ -84,23 +64,15 @@ namespace main_savitch_3
     used--;
   }
   sequence::size_type sequence::size( ) const
-  //   size_type size( ) const
-  //     Postcondition: The return value is the number of items in the sequence.
   {
     return used;
   }
   bool sequence::is_item( ) const
-  //     Postcondition: A true return value indicates that there is a valid
-  //     "current" item that may be retrieved by activating the current
-  //     member function (listed below). A false return value indicates that
-  //     there is no valid current item.
   {
     if(current_index < used) return true;
     else return false;
   }
   sequence::value_type sequence::current( ) const
-  //     Precondition: is_item( ) returns true.
-  //     Postcondition: The item returned is the current item in the sequence.
   {
     assert(is_item());
     return data[current_index];
