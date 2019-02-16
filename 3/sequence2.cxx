fc3670af90fcb6e8891ca306c0c71d91b8064d40
sajad1993gh <sajad1993gh@gmail.com>
2019-02-16 15:45:39 -0500

    more

--- 3/sequence2.cxx
+++ 3/sequence2.cxx
@@ -22,6 +22,7 @@
 
  namespace main_savitch_4
  {
+   const sequence::size_type sequence::DEFAULT_CAPACITY;
    sequence::sequence(size_type initial_capacity)
    {
    data = new value_type[initial_capacity];
@@ -29,6 +30,7 @@
    used = 0;
    current_index = 0;
    }
+
    sequence::sequence(const sequence& source)
    {
      data = new value_type[source.capacity];
@@ -36,23 +38,26 @@
      used = source.used;
      copy(source.data, source.data + used, data);
    }
+
    sequence::~sequence()
    {
      delete[] data;
    }
+
    void sequence::start()
    {
      current_index = 0;
    }
+
    //
    void sequence::advance()
    {
      assert(is_item());
      current_index++;
    }
+
    void sequence::insert(const value_type& entry)
    {
-     assert(size() < CAPACITY);
      size_type i;
      if(!is_item()) current_index = 0;
      for(i= used; i > current_index; i--){
@@ -61,36 +66,55 @@
      data[current_index]=entry;
      used++;
    }
+
    void sequence::attach(const value_type& entry)
    {
-     assert(size() < CAPACITY);
      size_type i;
      if(!is_item()) current_index = used-1;
      current_index++;
-     for(i= used; i > current_index; i--){
+     for(i= used; i > current_index; i--)
+     {
        data[i]=data[i-1];
      }
      data[current_index]=entry;
      used++;
    }
+
    void sequence::remove_current()
    {
      assert(is_item());
      size_type i;
-     for(i = current_index+1 ; i< used ; i++){
+     for(i = current_index+1 ; i< used ; i++)
+     {
        data[i-1]=data[i];
      }
      used--;
    }
-   void resize(size_type new_capacity);
-   void operator=(const sequence& source)
+
+   void resize(size_type new_capacity)
+   //library facility used: algorithm
+   {
+     value_type* larger_array;
+     if(new_capacity == capacity)
+      return;
+     if(new_capacity<used)
+      new_capacity == used;
+     larger_array = new value_type[new_capacity];
+     copy(data, data+used, larger_array)
+     delete[] data;
+     data = larger_array;
+     capacity = new_capacity;
+   }
+
+   void sequence::operator =(const sequence& source)
    //library facility used: algorithm
    {
      value_type *new_data;
      //check for possible self assignment:
      if(this == &source)
       return;
-     if(capacity != source.capacity){
+     if(capacity != source.capacity)
+     {
       new_data = new value_type[source.capacity];
       delete[] data;
       data = new_data;
@@ -99,6 +123,22 @@
      used = source.used;
      copy(source.data, source.data + used, data);
    }
+
+   sequence::size_type sequence::size( ) const
+   {
+     return used;
+   }
+   bool sequence::is_item( ) const
+   {
+     if(current_index < used) return true;
+     else return false;
+   }
+   sequence::value_type sequence::current( ) const
+   {
+     assert(is_item());
+     return data[current_index];
+   }
+
 
 
 
