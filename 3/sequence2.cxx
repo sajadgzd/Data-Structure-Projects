43e31ba192eaaf07872f728db176004c53656ca5
sajad1993gh <sajad1993gh@gmail.com>
2019-02-16 12:54:54 -0500

    more

---
+++ 3/sequence2.cxx
@@ -0,0 +1,23 @@
+ a constructor, start, insert, advance, and current
+
+ // Provided by:   ____________(Sajad Gholamzadehrizi)__________
+ // Email Address: ____________(sajad1993gh@gmail.com)_________
+ // FILE: sequence2.cxx
+ // CLASS IMPLEMENTED: sequence (see sequence2.h for documentation)
+ // INVARIANT for the sequence class:
+ //   1. The number of items in the sequence is stored in the member variable used;
+ //   2. For an empty sequence, we do not care what is stored in any of data; for a
+ //      non-empty sequence the items are stored in a sequence order from data[0] through
+ //      data[used-1], and we don't care what's in the rest of data.
+ //   3.If there is a current item, then it lies in data[current_index]; if there is
+ //   no current item, then current_index equals used.
+
+ #include <cassert>   // Provides assert function . k
+ using namespace std;
+
+ namespace main_savitch_4
+ {
+
+
+
+}
