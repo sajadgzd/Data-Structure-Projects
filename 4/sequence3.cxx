7c11655fac4e548835810a97d33e11d22b0c2eae
sajad1993gh <sajad1993gh@gmail.com>
2019-03-10 00:54:34 -0500

    more

--- 4/sequence3.cxx
+++ 4/sequence3.cxx
@@ -1,3 +1,8 @@
 //You will write all of this file, which will have the invariant of the sequence class,
 //the implementations of all the member functions of the sequence class,
 //and the Big-Os of these functions.
+//You should write the time analysis in the comment lines of each function.  The breakdowns of points (of 100) will be the followings:
+//Basis points (70) if your implementation passes the seq_ex3 exam
+//Invariant of the class (5 points)
+//Run time analysis (15 points)
+//Other implementation details such as compiling warning, code styles and efficiency, etc. (10 points)  
