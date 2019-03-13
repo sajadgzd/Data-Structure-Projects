8df42c78c1129bbb74881077d01f3efefbad9374
sajad1993gh <sajad1993gh@gmail.com>
2019-03-13 12:42:42 -0400

    more

--- 4/sequence3.cxx
+++ 4/sequence3.cxx
@@ -5,4 +5,20 @@
 //Basis points (70) if your implementation passes the seq_ex3 exam
 //Invariant of the class (5 points)
 //Run time analysis (15 points)
-//Other implementation details such as compiling warning, code styles and efficiency, etc. (10 points)  
+//Other implementation details such as compiling warning, code styles and efficiency, etc. (10 points)
+
+// Provided by:   ____________(Sajad Gholamzadehrizi)__________
+// Email Address: ____________(sajad1993gh@gmail.com)_________
+// FILE: sequence3.cxx
+// CLASS IMPLEMENTED: sequence (see sequence3.h for documentation)
+// INVARIANT for the sequence class:
+//   1. The number of items in the sequence is stored in the member variable used;
+//   2. If there is a current item, then it lies in data[current_index]; if there is
+//   no current item, then current_index equals used.
+//   3. The actual items of the sequence are stored in a partially filled dynamic array.
+//      The array is dynamic, pointed to by the member variable data.
+//      A larger dynamic array may be constructed if needed as the program runs.
+//    4. The total size of the dynamic array(current capacity) is in the member variable capacity.
+//    5.
+//    6.
+//
