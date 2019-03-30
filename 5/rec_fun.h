45c144feba78cb9e79cd24ebfcd4ae22f006809b
sajad1993gh <sajad1993gh@gmail.com>
2019-03-30 13:40:10 -0400

    more

--- 5/rec_fun.h
+++ 5/rec_fun.h
@@ -29,17 +29,21 @@
 // postcondition: print out the above pattern by calling number from m to n
 
 
-#ifndef rec_fun
-#define rec_fun
+#ifndef SAJAD_REC_H
+#define SAJAD_REC_H
 #include <iostream>
 #include <cstdlib>
 
 namespace sajad_rec
 {
+	class rec
+	{
+	public:
     void binary_print(ostream& outs, unsigned int n);
     void triangle(ostream& outs, unsigned int m, unsigned int n);
     double pow(double x, int n);
     void indented_sentences(size_t m, size_t n);
+	}
 }
 
 #endif
