49423a64efad0fff9ef167bce99f4a64a0b5a51a
sajad1993gh <sajad1993gh@gmail.com>
2019-03-30 19:32:44 -0400

    more

--- 5/rec_fun.cxx
+++ 5/rec_fun.cxx
@@ -9,7 +9,8 @@ using namespace std;
 namespace sajad_rec
 {
 
-  void rec::binary_print(ostream& outs, unsigned int n) {
+  void rec::binary_print(ostream& outs, unsigned int n)
+  {
     if (n == 0 || n == 1) cout << n;
     else {
       binary_print(outs, n/2);  // reduce n by order of 2
@@ -17,7 +18,8 @@ namespace sajad_rec
     }
   }
 
-  void rec::triangle(ostream& outs, unsigned int m, unsigned int n) {
+  void rec::triangle(ostream& outs, unsigned int m, unsigned int n)
+  {
     assert(m <= n);
     if(m == n) {
         for (int i = 0; i < m; i++) cout << "*";
@@ -31,7 +33,8 @@ namespace sajad_rec
       }
     }
 
-    double rec::pow(double x, int n) {
+    double rec::pow(double x, int n)
+    {
       if (x == 0) assert(n > 0);
       if (n == 0) return 1;
       if (n == 1) return x;
@@ -40,7 +43,8 @@ namespace sajad_rec
       else return (x*pow(x, n/2) * pow(x, n/2));
     }
 
-    void rec::indented_sentences(size_t m, size_t n) {
+    void rec::indented_sentences(size_t m, size_t n)
+    {
       assert(m <= n+1);
       if (m <= n) {
         for(size_t i = m; i > 1; i--) cout << " ";
