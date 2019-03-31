78f483f5bbe5fdecd155a83317e7a938443f534b
sajad1993gh <sajad1993gh@gmail.com>
2019-03-30 20:07:53 -0400

    more

--- 5/rec_fun_test.cpp
+++ 5/rec_fun_test.cpp
@@ -9,8 +9,8 @@ int main()
 {
 	rec recursion;
 
-	cout << "binary_print(cout, 27):"<<"\n";
-	recursion.binary_print(cout, 44);
+	cout << "binary_print(cout, 3):"<<"\n";
+	recursion.binary_print(cout, 3);
 
 	cout << "\nTriangle(cout, 3, 5):" << "\n";
 	recursion.triangle(cout, 3, 5);
