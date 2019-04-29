e032e44b9a85fc00e1523f73eea289c420292f0e
sajad1993gh <sajad1993gh@gmail.com>
2019-04-28 21:02:27 -0400

    more

--- 6/bag6.h
+++ 6/bag6.h
@@ -66,7 +66,7 @@ namespace main_savitch_10
     {
     public:
         // TYPEDEFS
-	typedef std::size_t size_type;
+	      typedef std::size_t size_type;
         typedef Item value_type;
         // CONSTRUCTORS and DESTRUCTOR
         bag( ) { root_ptr = NULL; }
